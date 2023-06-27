7feb2132223c3519c29560063b323c0fbef897a8
rahul1355 <47458498+rahul1355@users.noreply.github.com>
2023-06-27 11:23:28 +0530

    views of accounts for mannage-shipping

--- views.py
+++ views.py
@@ -1,65 +1,220 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from store.models import Product
-from .cart import Cart
-from django.http import JsonResponse
+from django.shortcuts import render,redirect
+from account.forms import CreateUserForm,LoginForm,UpdateUserForm
+from django.contrib.sites.shortcuts import get_current_site
+from .token import user_tokenizer_generate
+
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes,force_str
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+
+from django.contrib.auth.models import User
+
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
+
 
 # Create your views here.
-def cart_summary(request):
-    return render(request,'cart/cart-summary.html')
+def register(request):
 
-def cart_add(request):
-    cart = Cart(request)
+    form = CreateUserForm()
 
-    if request.POST.get('action') == 'post':
+    if request.method == 'POST':
 
-        product_id = int(request.POST.get('product_id'))
-        product_quantity = int(request.POST.get('product_quantity'))
+        form = CreateUserForm(request.POST)
 
-        product = get_object_or_404(Product, id=product_id)
+        if form.is_valid(): 
 
-        cart.add(product=product, product_qty=product_quantity)
+            user = form.save()
+
+            user.is_active = False
+
+            user.save()
+
+            # Email verification setup (template)
+
+            current_site = get_current_site(request)
+
+            subject = 'Account verification email'
+
+            message = render_to_string('account/registeration/email-verification.html', {
+            
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+            
+            })
+
+            user.email_user(subject=subject, message=message)
+
+
+            return redirect('email-verification-sent')
+
+
+
+    context = {'form':form}
+
+
+    return render(request, 'account/registeration/register.html', context=context)
+
+def email_verification(request, uidb64, token):
+
+    # uniqueid
+
+    unique_id = force_str(urlsafe_base64_decode(uidb64))
+
+    user = User.objects.get(pk=unique_id)
+    
+    # Success
+
+    if user and user_tokenizer_generate.check_token(user, token):
+
+        user.is_active = True
+
+        user.save()
+
+        return redirect('email-verification-success')
+
+
+    # Failed 
+
+    else:
+
+        return redirect('email-verification-failed')
+    
+def email_verification_sent(request):
+    return render(request,'account/registeration/email-verification-sent.html')
+
+def email_verification_failed(request):
+    return render(request,'account/registeration/email-verification-failed.html')
+    
+
+def email_verification_success(request):
+    return render(request,'account/registeration/email-verification-success.html')
+    
+
+# Login functionality
+
+def my_login(request):
+    form = LoginForm()
+    
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
         
-        cart_quantity = cart.__len__()
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect("dashboard")
+        
+    context = {
+        'form':form
+    }
+    
+    return render(request,"account/my-login.html",context=context)
 
-        response = JsonResponse({'qty': cart_quantity})
-
-        return response
-
-def cart_update(request):
-    cart = Cart(request)
-
-    if request.POST.get('action') == 'post':
-
-        product_id = int(request.POST.get('product_id'))
-        product_quantity = int(request.POST.get('product_quantity'))
-
-        cart.update(product=product_id, qty=product_quantity)
-
-
-        cart_quantity = cart.__len__()
-
-        cart_total = cart.get_total()
-
-        response = JsonResponse({'qty':cart_quantity, 'total':cart_total})
-
-        return response
-
-def cart_delete(request):
-    cart = Cart(request)
-
-    if request.POST.get('action') == 'post':
-
-        product_id = int(request.POST.get('product_id'))
-
-        cart.delete(product=product_id)
+@login_required(login_url='my-login')
+def dashboard(request):
+    
+    return render(request,"account/dashboard.html")
 
 
-        cart_quantity = cart.__len__()
 
-        cart_total = cart.get_total()
+@login_required(login_url='my-login')
+def profile_management(request):
+    
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST,instance=request.user)
+        
+        if user_form.is_valid():
+            user_form.save()
+            
+            messages.info(request, 'Account updated')
+            
+            return redirect('dashboard')
+    
+    user_form = UpdateUserForm(instance=request.user)
+    
+    context = {
+        'user_form':user_form
+    }
+    
+    return render(request,"account/profile-management.html",context=context)
 
 
-        response = JsonResponse({'qty':cart_quantity, 'total':cart_total})
 
-        return response 
+@login_required(login_url='my-login')
+def delete_account(request):
+    
+    user = User.objects.get(id=request.user.id)
+
+    if request.method == 'POST':
+        user.delete()
+        
+        messages.error(request, 'Account deleted')
+        
+        return redirect('store')
+    
+    return render(request,"account/delete-account.html")
+
+def user_logout(request):
+    
+    try:
+
+        for key in list(request.session.keys()):
+
+            if key == 'session_key':
+
+                continue
+
+            else:
+
+                del request.session[key]
+
+
+    except KeyError:
+
+        pass
+        
+    messages.success(request,"Logout success")
+    
+    return redirect('store')
+
+
+@login_required(login_url='my-login')    
+def manage_shipping(request):
+    try:
+        shipping = ShippingAddress.objects.get(user=request.user.id)
+        
+    except ShippingAddress.DoesNotExist:
+        shipping = None
+        
+    form = ShippingForm(instance=shipping)
+    
+    if request.method == 'POST':
+        form = ShippingForm(request.POST,instance=shipping)
+        
+        if form.is_valid():
+            # Assign FK on to user 
+            shipping_user = form.save(commit=False)
+            
+            # Adding FK itself
+            
+            shipping_user.user = request.user
+            
+            shipping_user.save()
+            
+            return redirect('dashboard')            
+    
+    context = {'form':form}
+    
+    return render(request,"account/manage-shipping.html",context=context)
